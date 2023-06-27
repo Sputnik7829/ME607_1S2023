801e87779155a371fbc46c9132b6e18be5511113
Henrique Toshio <111657538+Sputnik7829@users.noreply.github.com>
2023-06-27 13:47:26 -0300

    Update updateapp.R

--- updateapp.R
+++ updateapp.R
@@ -1,6 +1,17 @@
 #!/usr/bin/Rscript
 # Update APP
 install.packages('rsconnect')
+install.packages("commonmark")
+install.packages("httpuv") 
+install.packages("kableExtra")
+install.packages("later")
+install.packages("promises")
+install.packages("shiny")
+install.packages("shinythemes")
+install.packages("sourcetools")
+install.packages("svglite")
+install.packages("web")
+
 library(rsconnect)
 rsconnect::setAccountInfo(name='h172284',
                           token='2C6F9B54FB985E741B7204754EE2E723',
