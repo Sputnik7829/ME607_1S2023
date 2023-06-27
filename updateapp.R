8c29b5ac52dba6fe5678646e8010ebe0662ec307
Henrique Toshio <111657538+Sputnik7829@users.noreply.github.com>
2023-06-27 13:29:02 -0300

    Update updateapp.R

--- updateapp.R
+++ updateapp.R
@@ -2,5 +2,7 @@
 # Update APP
 install.packages('rsconnect')
 library(rsconnect)
-setAccountInfo(name=${{secrets.NAME}},token=${{secrets.TOKEN}},secret=${{secrets.SECRET}})
+rsconnect::setAccountInfo(name='h172284',
+                          token='2C6F9B54FB985E741B7204754EE2E723',
+                          secret='YeNESbbouskHtZIqBICsAUa5PfEzJiQnH1H8sqJ4')
 deployApp()
