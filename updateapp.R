24f2db40c68772f6f3147673fdf8a0dfa04415f9
Henrique Toshio <111657538+Sputnik7829@users.noreply.github.com>
2023-06-27 13:48:49 -0300

    Update updateapp.R

--- updateapp.R
+++ updateapp.R
@@ -13,7 +13,7 @@ install.packages("svglite")
 install.packages("web")
 
 library(rsconnect)
-rsconnect::setAccountInfo(name='h172284',
-                          token='2C6F9B54FB985E741B7204754EE2E723',
-                          secret='YeNESbbouskHtZIqBICsAUa5PfEzJiQnH1H8sqJ4')
+setAccountInfo(name='h172284',
+                token='2C6F9B54FB985E741B7204754EE2E723',
+                secret='YeNESbbouskHtZIqBICsAUa5PfEzJiQnH1H8sqJ4')
 deployApp()
